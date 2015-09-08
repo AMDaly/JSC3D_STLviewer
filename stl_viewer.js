80b0c929ba37426229e5db1ab2f9434bf33c58ec
unknown <amdaly625@gmail.com>
2015-09-07 23:36:33 -0400

    Including quality selection

--- stl_viewer.js
+++ stl_viewer.js
@@ -29,14 +29,14 @@ $(document).ready(function() {
 		canvas_drop.addEventListener('dragover', handleDragOver, false);
 		canvas_drop.addEventListener('drop', handleFileSelect, false);
 
-	var lowQualButton = document.getElementById('low_quality')
+	/*var lowQualButton = document.getElementById('low_quality')
 		lowQualButton.addEventListener('onclick', setDefinition('low'), false);
 
 	var standardQualButton = document.getElementById('low_quality')
     	standardQualButton.addEventListener('onclick', setDefinition('standard'), false);
 
     var highQualButton = document.getElementById('low_quality')
-    	highQualButton.addEventListener('onclick', setDefinition('high'), false);
+    	highQualButton.addEventListener('onclick', setDefinition('high'), false);*/
 
 	//Drag and drop logic:
 	function handleFileSelect(evt) {
